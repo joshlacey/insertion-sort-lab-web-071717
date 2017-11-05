6b86f67f50a61923f6f2a1f602a05a671e630899
joshlacey <williamjoshualacey@gmail.com>
2017-11-05 14:36:46 -0500

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 function findMinAndRemove(array){
-
+  let mindex = 0;
+  for(let i = 1; i < array.length; i++) {
+    (array[i] < array[mindex]) && (mindex = i)
+  };
+  return array.splice( mindex, 1 )[0];
 }
 
 function insertionSort(array){
-
+  let newArray = []
+  let length = array.length
+  for(let i=0; i < length; i++) {
+    newArray[i] = findMinAndRemove(array)
+  }
+  return newArray
 }
